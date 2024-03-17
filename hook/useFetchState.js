8d6ae2294b1d0f8d0fdfb38b9040bfafd1fd73a3
简尚全 <jianshangquan3@gmail.com>
2024-03-17 14:22:48 +0630

    update fetchState

--- hook/useFetchState.js
+++ hook/useFetchState.js
@@ -17,13 +17,15 @@ module.exports.useFetchState = function useFetchState(){
         progress: 0,
     });
 
-    return {
+    const func = {
         status: state,
         notInitialize: () => setState({ status: FetchState.NOT_INITIALIZE, data: null, error: null, progress: 0, message: '' }),
         completed: ({data, message} = {}) => setState({ status: FetchState.COMPLETED, data, error: null, progress: 100, message }),
-        error: ({error, message} = {}) => setState({ status: FetchState.ERROR, data: null, error, progress: 0, message }),
+        error: ({error, message} = {}) => setState({ status: FetchState.ERROR, data: null, error, progress: 0, message: message || error.message }),
+        pending: ({ progress = 0, message }) => func.loading(progress, message),
         loading: (progress = 0, message) => setState({ status: FetchState.LOADING, data: null, error: null, progress, message }),
     }
+    return func;
 }
 
 
