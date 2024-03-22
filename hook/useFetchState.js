5dd0f1efe00324fbe65e9327ded372dc0ea8918a
简尚全 <jianshangquan3@gmail.com>
2024-03-23 01:30:53 +0630

    add useFetchState with auto close

--- hook/useFetchState.js
+++ hook/useFetchState.js
@@ -1,4 +1,4 @@
-const { useState } = require('react');
+const { useState, useRef, useEffect } = require('react');
 
 const FetchState = Object.freeze({
     LOADING: 'loading',
@@ -9,6 +9,7 @@ const FetchState = Object.freeze({
 module.exports.FetchState =  FetchState;
 
 module.exports.useFetchState = function useFetchState(){
+    const timer = useRef();
     const [state, setState] = useState({
         status: FetchState.NOT_INITIALIZE,
         data: null,
@@ -17,10 +18,26 @@ module.exports.useFetchState = function useFetchState(){
         progress: 0,
     });
 
+
+
+    useEffect(() => {
+        return () => {
+            clearTimeout(timer.current);
+        }
+    }, [])
+
     const func = {
         status: state,
         notInitialize: () => setState({ status: FetchState.NOT_INITIALIZE, data: null, error: null, progress: 0, message: '' }),
-        completed: ({data, message} = {}) => setState({ status: FetchState.COMPLETED, data, error: null, progress: 100, message }),
+        completed: ({data, message, autoSetNotInitializedTimeout = null} = {}) => {
+            setState({ status: FetchState.COMPLETED, data, error: null, progress: 100, message })
+            if(autoSetNotInitializedTimeout != null){
+                clearTimeout(timer.current);
+                timer.current = setTimeout(() => {
+                    func.notInitialize();
+                }, autoSetNotInitializedTimeout);
+            }
+        },
         error: ({error, message} = {}) => setState({ status: FetchState.ERROR, data: null, error, progress: 0, message: message || error.message }),
         pending: ({ progress = 0, message }) => func.loading(progress, message),
         loading: (progress = 0, message) => setState({ status: FetchState.LOADING, data: null, error: null, progress, message }),
